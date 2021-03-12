c5306d45708e86f7dbb814d9c15a2e178d94ff29
Garrett Smith <g.smith61900@gmail.com>
2021-03-11 21:26:24 -0700

    webgl commit

---
+++ GS_M02_DV01_02_NavMesh/Assets/Scripts/DoorOpener.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoorOpener : MonoBehaviour
+{
+    bool open = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            if (open)
+                this.transform.Translate(0, -3f, 0);
+            else
+                this.transform.Translate(0, 3f, 0);
+
+            open = !open;
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # GS_M02_DV01_NavMesh
  
+Link to playable: https://itsgarbear.github.io/GS_M02_DV01_NavMesh
